20625ee9a4cd7a3bbff1b300085579ec5973bc28
Angelo Al Yacoub <aalyacoub@alteca.fr>
2023-10-19 09:46:00 +0200

    Copy of the LazyVim config for neovim with my (few) tweaks

---
+++ lua/plugins/tmux.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+      { "<C-h>", "<cmd> TmuxNavigateLeft<CR>", desc = "Move left Tmux" },
+      { "<C-l>", "<cmd> TmuxNavigateRight<CR>", desc = "Move right Tmux" },
+      { "<C-j>", "<cmd> TmuxNavigateDown<CR>", desc = "Move down Tmux" },
+      { "<C-h>", "<cmd> TmuxNavigateUp<CR>", desc = "Move up Tmux" },
+    },
+  },
+}
